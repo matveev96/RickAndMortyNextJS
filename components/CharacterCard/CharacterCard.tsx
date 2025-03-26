75963dd6f83eb79e0b064db180a3d041dc73c7eb
matveev96 <sasha.matveev1996@icloud.com>
2025-03-26 21:36:38 +0300

    feat: realize CharacterCard

---
+++ components/CharacterCard/CharacterCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Image from "next/image";
+import {type CharacterType} from "@/assets/hooks/useCharacters";
+import styles from './CharactersCard.module.scss'
+
+export const CharacterCard = (props: {character: CharacterType}) => {
+    const {character} = props
+    return (
+        <div>
+            <div className={styles.characterCard}>
+                <p>{character.name}</p>
+                <Image src={character.image} alt={`Picture of ${character.name}`} width={300} height={300}/>
+            </div>
+        </div>
+    );
+};
