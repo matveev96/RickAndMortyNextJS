9a9b9df532008ac6e934e3e475a3474938a5f417
matveev96 <sasha.matveev1996@icloud.com>
2025-03-27 09:49:04 +0300

    feat: add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {getLayout} from "@/components/Layout/Layout";
+
+const NotFound = () => {
+    return (
+        <div>
+            404 Page Not Found
+        </div>
+    );
+};
+
+NotFound.getLayout = getLayout;
+export default NotFound;
