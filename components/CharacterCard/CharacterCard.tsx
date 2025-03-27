3b1889c25878cce0a24d19e48b922b642db19801
matveev96 <sasha.matveev1996@icloud.com>
2025-03-27 09:55:04 +0300

    fix: fix structure of CharacterCard

--- components/CharacterCard/CharacterCard.tsx
+++ components/CharacterCard/CharacterCard.tsx
@@ -6,11 +6,9 @@ import styles from './CharactersCard.module.scss'
 export const CharacterCard = (props: {character: CharacterType}) => {
     const {character} = props
     return (
-        <div>
             <div className={styles.characterCard}>
                 <p>{character.name}</p>
                 <Image src={character.image} alt={`Picture of ${character.name}`} width={300} height={300}/>
             </div>
-        </div>
     );
 };
