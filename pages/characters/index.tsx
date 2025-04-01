eb315cba56d29bcfeaeae565874f534a86a668af
matveev96 <sasha.matveev1996@icloud.com>
2025-04-01 16:59:25 +0300

    feat: add getStaticProps instead useCharacters with useEffect

--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -1,22 +1,43 @@
-import {useCharacters} from "@/assets/hooks/useCharacters";
 import {CharacterCard} from "@/components/CharacterCard/CharacterCard";
 import {HeadMeta} from "@/components/HeadMeta/HeadMeta";
 import {getLayout} from "@/components/Layout/Layout";
 import Link from "next/link";
+import {API} from "@/assets/api/api";
+import type {CharacterType, ResponseType} from "@/assets/api/rick-and-morty-api";
+import styles from "@/styles/Home.module.css";
 
 
-function Characters() {
+export const getStaticProps = async () => {
+    const characters = await API.rickAndMorty.getCharacters()
+    return {
+        props: {
+            characters,
+        }
+    }
+}
 
-    const characters = useCharacters()
+type PropsType = {
+    characters: ResponseType<CharacterType>
+}
 
+function Characters(props: PropsType) {
+
+    // const characters = useCharacters() если используем useEffect (работа как с обычным React)
+
+    const {characters} = props;
+
+    const characterList = characters.results.map(character => (
+        <Link href={`/characters/${character.id}`} key={character.id}>
+            <CharacterCard character={character}/>
+        </Link>
+    ))
     return (
         <>
             <HeadMeta title={"Characters"}/>
-            {characters && characters.map(character => (
-                <Link href={`/characters/${character.id}`} key={character.id}>
-                    <CharacterCard  character={character}/>
-                </Link>
-            ))}
+            <div className={styles.cards}>
+                {characterList}
+            </div>
+
         </>
     );
 }
