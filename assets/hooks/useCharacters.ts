b65f2cf21d950f380a235d196706d048bbad51be
matveev96 <sasha.matveev1996@icloud.com>
2025-04-01 17:01:26 +0300

    refactor: comment useCharacters so use getStaticProps in project

--- assets/hooks/useCharacters.ts
+++ assets/hooks/useCharacters.ts
@@ -1,20 +1,20 @@
-import {useEffect, useState} from "react";
-import type {Nullable} from "@/assets/types/Nullable";
-import {API} from "@/assets/api/api";
-
-export const useCharacters = (): Nullable<CharacterType[]> => {
-    const [characters, setCharacters] = useState<Nullable<CharacterType[]>>(null);
-
-    useEffect(() => {
-        API.rickAndMorty.getCharacters().then(res => setCharacters(res.results));
-    }, [])
-
-    return characters;
-}
-
-//types
-export type CharacterType = {
-    name: string;
-    id: number;
-    image: string;
-}
+// import {useEffect, useState} from "react";
+// import type {Nullable} from "@/assets/types/Nullable";
+// import {API} from "@/assets/api/api";
+//
+// export const useCharacters = (): Nullable<CharacterType[]> => {
+//     const [characters, setCharacters] = useState<Nullable<CharacterType[]>>(null);
+//
+//     useEffect(() => {
+//         API.rickAndMorty.getCharacters().then(res => setCharacters(res.results));
+//     }, [])
+//
+//     return characters;
+// }
+//
+// //types
+// export type CharacterType = {
+//     name: string;
+//     id: number;
+//     image: string;
+// }
