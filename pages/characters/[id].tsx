059cb3c020e7a3c7d30165b5c8414da8583c0c26
matveev96 <sasha.matveev1996@icloud.com>
2025-04-02 16:36:23 +0300

    refactor: add getStaticPaths and getStaticProps

--- pages/characters/[id].tsx
+++ pages/characters/[id].tsx
@@ -1,11 +1,49 @@
 import {CharacterCard} from "@/components/CharacterCard/CharacterCard";
 import {HeadMeta} from "@/components/HeadMeta/HeadMeta";
 import {getLayout} from "@/components/Layout/Layout";
-import {useCharacter} from "@/assets/hooks/useCharacter";
+import type {GetStaticPaths, GetStaticProps} from "next";
+import {API} from "@/assets/api/api";
+import type {CharacterType} from "@/assets/api/rick-and-morty-api";
 
-function Character() {
+export const getStaticPaths: GetStaticPaths = async () => {
 
-    const character = useCharacter()
+    const {results} = await API.rickAndMorty.getCharacters()
+
+    const paths = results.map(character => {
+        return {params: {id: String(character.id)}}
+    })
+
+    return {
+        paths,
+        fallback: true,
+    }
+}
+
+export const getStaticProps: GetStaticProps = async ({params}) => {
+    const {id} = params || {}
+
+    const character = await API.rickAndMorty.getCharacter(id as string)
+
+    if(!character) {
+        return {
+            notFound: true,
+        }
+    }
+
+    return {
+        props: {
+            character
+        }
+    }
+}
+
+type PropsType = {
+    character: CharacterType
+}
+
+function Character({ character }: PropsType) {
+
+    // const character = useCharacter() если используем useEffect (работа как с обычным React)
 
     return (
         <>
