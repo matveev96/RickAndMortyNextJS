71b74c30cc7a20f943f75505a954defe7134895a
matveev96 <sasha.matveev1996@icloud.com>
2025-04-02 16:36:41 +0300

    refactor: comment useCharacter

--- assets/hooks/useCharacter.ts
+++ assets/hooks/useCharacter.ts
@@ -1,17 +1,17 @@
-import {useEffect, useState} from "react";
-import type {CharacterType} from "@/assets/hooks/useCharacters";
-import {useRouter} from "next/router";
-import type {Nullable} from "@/assets/types/Nullable";
-import {API} from "@/assets/api/api";
-
-export const useCharacter = (): Nullable<CharacterType> => {
-
-    const [character, setCharacter] = useState<Nullable<CharacterType>>(null);
-    const router = useRouter();
-
-    useEffect(() => {
-        API.rickAndMorty.getCharacter(router.query.id).then(res => setCharacter(res))
-    }, [])
-
-    return character;
-}
+// import {useEffect, useState} from "react";
+// import type {CharacterType} from "@/assets/hooks/useCharacters";
+// import {useRouter} from "next/router";
+// import type {Nullable} from "@/assets/types/Nullable";
+// import {API} from "@/assets/api/api";
+//
+// export const useCharacter = (): Nullable<CharacterType> => {
+//
+//     const [character, setCharacter] = useState<Nullable<CharacterType>>(null);
+//     const router = useRouter();
+//
+//     useEffect(() => {
+//         API.rickAndMorty.getCharacter(router.query.id).then(res => setCharacter(res))
+//     }, [])
+//
+//     return character;
+// }
