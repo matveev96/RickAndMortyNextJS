96079e472ef58474e0546701fda2e0c7a63c8d48
matveev96 <sasha.matveev1996@icloud.com>
2025-04-04 13:47:04 +0300

    feat: add statuses to CharacterCard

--- components/CharacterCard/CharacterCard.tsx
+++ components/CharacterCard/CharacterCard.tsx
@@ -2,13 +2,28 @@ import React from 'react';
 import Image from "next/image";
 import styles from './CharactersCard.module.scss'
 import type {CharacterType} from "@/assets/api/rick-and-morty-api";
+import aliveStatus from 'public/statuses/alive.png'
+import deadStatus from 'public/statuses/dead.png'
+import unknownStatus from 'public/statuses/unknown.png'
+import Status from "@/components/Status/Status";
+
+const statusImage = {
+    Alive: aliveStatus,
+    Dead: deadStatus,
+    unknown: unknownStatus,
+}
 
 export const CharacterCard = (props: {character: CharacterType}) => {
-    const {character} = props
+    const {image, name, status} = props.character
+
     return (
             <div className={styles.characterCard}>
-                <p>{character.name}</p>
-                <Image src={character.image} alt={`Picture of ${character.name}`} width={300} height={300}/>
+                <div className={styles.title}>
+                    <p>{name}</p>
+                    <Status src={statusImage[status]}/>
+                </div>
+
+                <Image src={image} alt={`Picture of ${name}`} width={300} height={300}/>
             </div>
     );
 };
