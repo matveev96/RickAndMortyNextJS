823152cda53be11d274707914a061e5e8f63704d
matveev96 <sasha.matveev1996@icloud.com>
2025-04-04 17:39:14 +0300

    feat: add pagination on page

--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -5,27 +5,44 @@ import Link from "next/link";
 import {API} from "@/assets/api/api";
 import type {CharacterType, ResponseType} from "@/assets/api/rick-and-morty-api";
 import styles from "@/styles/Home.module.css";
-import {GetStaticProps} from 'next'
+import {type GetServerSideProps} from 'next'
+import {useRouter} from "next/router";
 
 
-export const getStaticProps: GetStaticProps = async () => {
-    const characters = await API.rickAndMorty.getCharacters()
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const headers = new Headers({ 'Cache-Control': 'public, s-maxage=10, stale-while-revalidate=100' });
+    context.res.setHeaders(headers)
+
+    const page = Number(context.query.page) || 1;
+
+    const characters = await API.rickAndMorty.getCharacters({page})
     return {
         props: {
             characters,
+            page
         }
     }
 }
 
 type PropsType = {
-    characters: ResponseType<CharacterType>
+    characters: ResponseType<CharacterType>,
+    page: number,
 }
 
 function Characters(props: PropsType) {
 
     // const characters = useCharacters() если используем useEffect (работа как с обычным React)
 
-    const {characters} = props;
+    const {characters, page} = props;
+
+    const router = useRouter();
+
+    const handlePagination = (newPage: number) => {
+
+        if(newPage >= 1 && newPage <= 42) {
+            router.push(`?page=${newPage}`);
+        }
+    };
 
     const characterList = characters.results.map(character => (
         <Link href={`/characters/${character.id}`} key={character.id}>
@@ -35,10 +52,14 @@ function Characters(props: PropsType) {
     return (
         <>
             <HeadMeta title={"Characters"}/>
+            <div className={styles.buttons}>
+                <button onClick={() => handlePagination(page - 1)}>Previous</button>
+                <button onClick={() => handlePagination(1)}>Go to 1 page</button>
+                <button onClick={() => handlePagination(page + 1)}>Next</button>
+            </div>
             <div className={styles.cards}>
                 {characterList}
             </div>
-
         </>
     );
 }
