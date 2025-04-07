0b95f54d8b96ec057f67dba1e28960ebb47d58d7
matveev96 <sasha.matveev1996@icloud.com>
2025-04-07 21:56:26 +0300

    feat: add styles to Card and remove styledComponents

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from "react";
-import styled from "styled-components";
+import s from './Card.module.css'
 
 
 type PropsType = {
@@ -10,23 +10,9 @@ export const Card = (props: PropsWithChildren<PropsType>) => {
   const { children, name } = props;
 
   return (
-    <CardBlock>
-      <Name>{name}</Name>
+    <div className={s.cardBlock}>
+      <p className={s.name}>{name}</p>
       {children}
-    </CardBlock>
+    </div>
   );
 };
-
-const CardBlock = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 15px;
-  border: 2px solid #facaff;
-  box-shadow: 0 2px 3px 1px #fa52d3;
-  border-radius: 15px;
-`;
-
-const Name = styled.div`
-  font-weight: 600;
-  font-size: 24px;
-`;
